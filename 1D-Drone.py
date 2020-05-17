109c7240f41447cee453198a6f96e86d8df0d934
VaradVNIT <vaidyavarad2001@gmail.com>
2020-05-17 19:09:27 +0530

    Updated PID Control

--- 1D-Drone.py
+++ 1D-Drone.py
@@ -5,13 +5,17 @@ import matplotlib.pyplot as plt
 from scipy.integrate import odeint
 
 '''
-This project aims to develop a 1-D drone control which is controlled by PID and may be extended to other forms of controllers.
+This project aims to develop a 1-D drone control which is controlled by PID and may be
+extended to other forms of controllers.
 
 The controller will control the z direction of the drone.
 
 The dymanic model for the same can be written as:
 
 m * z_dbldot = - m * g + u ;
+
+for the odeint function the model used is
+z_dbldot = - g + u/m
 
 where u is the thurst input controlled by the controller
 
@@ -20,7 +24,12 @@ where u is the thurst input controlled by the controller
 # define the constants 
 m = 0.5  # kg
 g = 9.81 # m/s2
-simulation_time = 5
+
+# PID tuning parameters
+Kp = 50
+Kd = 10
+# Simulation Time Parameters
+simulation_time = 5 # seconds
 time_points = simulation_time * 100 + 1
 #define the function model
 
@@ -37,9 +46,11 @@ x0 = [0,0]
 # set the timepoints
 t = np.linspace(0,simulation_time,time_points)
 
-# give the step input
+delta_t = t[1] - t[0] # The time difference between the two time points
+
+# give the  to the model input
 u = np.zeros(time_points)
-u[101: ] = m * g
+#u[101: ] = m * g
 
 # Arrays to store the solutions
 z = np.empty_like(t)
@@ -48,10 +59,27 @@ zdot = np.empty_like(t)
 # Record the intial condition in the new solution arrays
 z[0],zdot[0] = x0
 
+# Set the desired z values
+# Sets the desired position and velocity to the values inside. The desired values now can change with time.
+zdes = np.vstack( (np.empty_like(t) , np.empty_like(t)) )       
+zdes[0] = np.ones(t.size)
+zdes[1] = np.zeros(t.size)
+print(zdes)
+# zdes[0] is the desired postion and zdes[1] is desired the velocity
+
+# Setiing up the error array
+error = np.empty_like(zdes)
+error[:,0] = zdes[:,0] - x0
+
+# The error[0] is position error and error[1] is the velocity error
+
 #solve the ODE
 
 for i in range(1,time_points):
     tspan = [t[i-1],t[i]]
+    error[:,i] = zdes[:,i] - x0  # x0 contains the current state for the error calculation
+
+    u[i] =  m * ( g + Kp * error[0][i] + Kd * error[1][i])
 
     x = odeint(zsolver,x0,t,args=(u[i],))
 
@@ -61,11 +89,10 @@ for i in range(1,time_points):
     # giving the next initial condition to the solver
     x0 = x[1]
 
-# Hopefully this will solve the problem
 
 # Plot the result
 
-plt.plot(t,u,'g:',label = 'u(t)')
+plt.plot(t,zdes[0],'g:',label = 'z desired')
 plt.plot(t,z,'r-',label = 'z(t)')
 plt.plot(t,zdot,'b--', label = 'zdot(t)')
 plt.xlabel('Time')
