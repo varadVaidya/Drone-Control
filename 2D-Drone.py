90698a118be9c4923df13f272348b1fb34646e64
Varad <vaidyavarad2001@gmail.com>
2020-08-18 19:33:25 +0530

    Added Trajectory Planning Class

--- 2D-Drone.py
+++ 2D-Drone.py
@@ -12,7 +12,7 @@ Point to Point Striaght Line Trajectory Planning Done
 '''
 '''
 This project aims to develop a 2D quadcoptor controller.
-The controlled planned to control the quadcoptor model are:
+The controller planned to control the quadcoptor model are:
 1. PID
 2. LQR
 3. MPC (Maybe)
@@ -43,6 +43,7 @@ class Quadrotor:
                                          np.random.randint(-5,5), np.random.uniform(-np.pi/3,np.pi/3)  ,0,0,0]) 
         # self.initial_state = np.array([np.random.randint(-5,5), 
         #                                  np.random.randint(-5,5), 0 ,0,0,0])   
+        #z,y,phi,zdot,ydot,phidot.
     
         #the initial state vector contains all the state and their dervitaive
         #The self.initial_state wil be a 1*6 vector.
@@ -77,6 +78,128 @@ class Quadrotor:
     #         solvedState = np.append(solvedState,self.current_state, axis=0)
     #     return solvedState
  
+class TrajectoryPlan:
+    
+    def __init__(self):
+        pass
+    
+    @staticmethod    
+    def minVelPath(Drone,des_state,plan_t,t):
+        initial_state = [Drone.initial_state[0],Drone.initial_state[1],0,0,0,0]
+        #copy the inital state in the format of z,y,zdot,ydot,zddot,yddot.
+        final_time = plan_t[-1]
+        #take the last element of the planning time array
+        
+        stateMatrix = np.empty([len(t),6])
+        stateMatrix[0,:] = initial_state 
+        stateMatrix[len(plan_t),:] = des_state # at the planned time the final state should be the desired state.
+        '''
+        We will be implementing shortest path or minimum velocity path
+        
+        So the trajectory is defined by 
+        x(t) = c_1 * t + c_0
+        
+        constrained to the condition x(0) is the intial state and x(T) is the final condition.
+        this reduces the problem to find out the coefficients c_0 and c_1 found by algebra
+        
+        '''
+        C_z_0 = Drone.initial_state[0]
+        C_z_1 = ( des_state[0] - C_z_0 )/final_time
+        
+        C_y_0 = Drone.initial_state[1]
+        C_y_1 = ( des_state[1] - C_y_0 )/final_time
+        
+        for i in range(1,len(t)):
+            
+            if ( i < len(plan_t) ):        
+                z = C_z_1 * t[i] + C_z_0
+                y = C_y_1 * t[i] + C_y_0
+                
+                vel_z = 0
+                vel_y = 0
+                
+                # vel_z = C_z_1
+                # vel_y = C_y_1
+                
+                stateMatrix[i,:] = [z,y,vel_z,vel_y,0,0]
+                
+            if (i > len(plan_t)):
+                stateMatrix[i,:] = des_state
+        
+        return stateMatrix
+    
+    @staticmethod
+    def minAccelnPath(Drone,des_state,plan_t,t):
+        '''
+        The trajectory for the minimum acceleration path is defined by a cubic polynomial with
+        constrains on initial and final ; position and velocity.
+        
+        z(t) = Az * t^3 + Bz * t^2 + Cz * t + Dz
+        
+        and the constraints are 
+        z(0) and z'(0) = initial_state
+        z(T) and z'(T) = final_state
+        
+        
+        z(0) = A * 0 + B* 0 + C* 0 + D
+        z'(0) = A * 3 0 ^2 + B * 2 0 + C  + 0 D
+        z(T) = A T^3 + B T^2 + C T + D        
+        z'(T) = A * 3T^2 + B * 2T + C  + 0 D
+        
+        which can be written as 
+        z(0)        0 0 0 1                     A
+        z'(0)   =   0 0 1 0             *       B
+        z(T)        T^3 T^2 T 1                 C
+        z'(T)       3T^2 2T 1 0                 D
+        the coeffiecnts can be obtained by using simple linear alegbra.
+        
+        Similar thing is done to the Y axis to find out the desired trajectory 
+        
+        '''
+        
+        final_time = plan_t[-1] # time for plannig.
+        print(final_time)
+        stateMatrix = np.empty([len(t),6])
+        #statematrix is the desired states at all time points which the controller will follow
+        # in the format of z,y,zdot,ydot,zddot,yddot.
+        
+        coeffMatrix = np.array([
+            [0,0,0,1],
+            [0,0,1,0],
+            [pow(final_time,3),pow(final_time,2),final_time,1],
+            [3*pow(final_time,2),2*final_time,1,0]
+        ])
+                
+        # z_constriants = np.array([Drone.initial_state[0],Drone.initial_state[3],des_state[0],des_state[2]])
+        # y_constriants = np.array([Drone.initial_state[1],Drone.initial_state[4],des_state[1],des_state[3]])
+        
+        z_constriants = np.array([Drone.initial_state[0],0,des_state[0],0]) #z(0),z(T),zdot(0),zdot(T)
+        y_constriants = np.array([Drone.initial_state[1],0,des_state[1],0]) #y(0),y(T),ydot(0),ydot(T)
+        
+        z_coeff = np.linalg.solve(coeffMatrix,z_constriants)                #solve for the coefficients
+        print("The Z coeff are: ",z_coeff,"And its constraints are ", z_constriants)
+        y_coeff = np.linalg.solve(coeffMatrix,y_constriants)                #solve for the coeffients
+        print("The Y coeff are: ",y_coeff,"And its constraints are ", y_constriants)
+        
+        stateMatrix[0,:] = [Drone.initial_state[0],Drone.initial_state[1],0,0,0,0]
+        
+        for i in range(1,len(t)):
+            
+            if( i <= len(plan_t) ):
+                z = z_coeff[0] * pow(t[i],3) + z_coeff[1] * pow(t[1],2) + z_coeff[2] * t[i] + z_coeff[3]
+                y = y_coeff[0] * pow(t[i],3) + y_coeff[1] * pow(t[1],2) + y_coeff[2] * t[i] + y_coeff[3]
+                zvel = 3 * z_coeff[0] * pow(t[i],2) + 2 * z_coeff[1] * t[i] + z_coeff[2]
+                yvel = 3 * y_coeff[0] * pow(t[i],2) + 2 * y_coeff[1] * t[i] + y_coeff[2]
+                
+                stateMatrix[i,:] = [z,y,zvel,yvel,0,0]
+                
+            if( i > len(plan_t)):
+                stateMatrix[i,:] = des_state
+            
+        
+        return stateMatrix
+        
+    
 def simulate(TUNING_MATRIX,des_stateMatrix,t):
     solvedState = Drone.initial_state
     timepoints = len(t)
@@ -134,11 +257,13 @@ def plotResults(solvedState,des_stateMatrix,t):
     ax2.set_title('Z Trajectory')
     ax2.grid(True)
     ax2.plot(t,solvedState[:,0],'r:',label = 'Z-axis')
+    ax2.plot(t,des_stateMatrix[:,0],'m:',label = 'Desired Z Trajectory')
     
     ax3 = fig.add_subplot(spec[1,3])
     ax3.set_title('Y Trajectory')
     ax3.grid(True)
     ax3.plot(t,solvedState[:,1],'g:',label = 'Y-axis')
+    ax3.plot(t,des_stateMatrix[:,1],'m:',label = 'Desired Y Trajectory')
     
     ax4 = fig.add_subplot(spec[2,3])
     ax4.set_title('Phi Trajectory')
@@ -148,51 +273,6 @@ def plotResults(solvedState,des_stateMatrix,t):
     
     
     return None
-
-def TrajectoryPlan(Drone,des_state,plan_t,t):
-    initial_state = [Drone.initial_state[0],Drone.initial_state[1],0,0,0,0]
-    final_time = plan_t[-1]
-    
-    stateMatrix = np.empty([len(t),6])
-    stateMatrix[0,:] = initial_state
-    stateMatrix[len(plan_t),:] = des_state
-    '''
-    We will be implementing shortest path or minimum velocity path
-    
-    So the trajectory is defined by 
-    x(t) = c_1 * t + c_0
-    
-    constrained to the condition x(0) is the intial state and x(T) is the final condition.
-    this reduces the problem to find out the coefficients c_0 and c_1 found by algebra
-    
-    '''
-    C_z_0 = Drone.initial_state[0]
-    C_z_1 = ( des_state[0] - C_z_0 )/final_time
-    
-    C_y_0 = Drone.initial_state[1]
-    C_y_1 = ( des_state[1] - C_y_0 )/final_time
-    
-    for i in range(1,len(t)):
-        
-        if ( i < len(plan_t) ):        
-            z = C_z_1 * t[i] + C_z_0
-            y = C_y_1 * t[i] + C_y_0
-            
-            vel_z = 0
-            vel_y = 0
-            
-            # vel_z = C_z_1
-            # vel_y = C_y_1
-            
-            stateMatrix[i,:] = [z,y,vel_z,vel_y,0,0]
-            
-        if (i > len(plan_t)):
-            stateMatrix[i,:] = des_state
-                    
-            
-    
-    return stateMatrix
-
 
 def anime():
     l = (Drone.Ixx * 12/ Drone.MASS) * (1 / 2)                  # length of drone
@@ -251,8 +331,8 @@ def anime():
     
 
     ani = animation.FuncAnimation(fig, animate,
-                                  interval=6.667, blit=False, init_func=init, repeat = False,frames=1000)
-    ani.save('2D.mp4',writer='ffmpeg',fps=25,bitrate=1800)
+                                  interval = 1, blit=False, init_func=init, repeat = False, frames=len(t))
+    ani.save('2D.mp4',writer='ffmpeg',fps=100,bitrate=1800)
 
     plt.show()
     return None
@@ -260,7 +340,7 @@ def anime():
 
 # Simulation Time Parameters
 simulation_time = 12 # seconds
-plan_time = 8
+plan_time = 8 #time for which the path has to be planned
 time_points = simulation_time * 100 + 1
 plan_time_points = plan_time * 100 + 1
 t = np.linspace(0,simulation_time,time_points)
@@ -284,13 +364,16 @@ TUNING_MATRIX = np.array([
 #     [25,10,0]    
 # ])
 
-des_stateMatrix = TrajectoryPlan(Drone,des_state,plan_t,t)
-print("Trajectory Planning Done")
+#des_stateMatrix = TrajectoryPlan.minVelPath(Drone,des_state,plan_t,t)
+
+des_stateMatrix = TrajectoryPlan.minAccelnPath(Drone,des_state,plan_t,t)
+print(np.shape(des_stateMatrix))
+print("Trajectory Planning Done. Solving for the states")
 #np.savetxt('check.csv',des_stateMatrix,fmt='%f',delimiter=",")
 solvedState = simulate(TUNING_MATRIX,des_stateMatrix,t)
-print("Solving the state done")
+print("Simulation Done. Animating the Plot")
 
-anime()
+#anime()
 plotResults(solvedState,des_stateMatrix,t)
-
+print(t)
         
